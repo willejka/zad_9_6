637f11b794cdc8d0d759161f0e7e021dd5abe43c
Willejka <ewasinska@gmail.com>
2018-02-18 21:32:05 +0100

    Add button Hello

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 //scripts.js
-function Phone(brand, price, color, size) {
-	this.brand = brand;
-	this.price = price;
-	this.color = color;
-  	this.size = size;
+function Button(text) {
+	this.text = text || 'Hello';
 }
-Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + ", size is " + this.size + " and the price is " + this.price + ".");
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
 }
+var btn1 = new Button('Hello!');
 
-var iPhone6S = new Phone("Apple", 2250, "silver", "big");
-var samsungGalaxyS6 = new Phone("Samsung", 1250, "white", "medium");
-var onePlusOne = new Phone("One Plus", 950, "grey", "small");
-
-iPhone6S.printInfo();
-samsungGalaxyS6.printInfo();
-onePlusOne.printInfo();
+btn1.create();
