a65682dbf897abd54b5626ffe4b522277ffe4899
Willejka <ewasinska@gmail.com>
2018-02-18 12:32:30 +0100

    Add size

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 //scripts.js
-function Phone(brand, price, color) {
+function Phone(brand, price, color, size) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
+  	this.size = size;
 }
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + ", size is " + this.size + " and the price is " + this.price + ".");
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver");
-var samsungGalaxyS6 = new Phone("Samsung", 1250, "white");
-var onePlusOne = new Phone("One Plus", 950, "grey");
+var iPhone6S = new Phone("Apple", 2250, "silver", "big");
+var samsungGalaxyS6 = new Phone("Samsung", 1250, "white", "medium");
+var onePlusOne = new Phone("One Plus", 950, "grey", "small");
 
 iPhone6S.printInfo();
 samsungGalaxyS6.printInfo();
